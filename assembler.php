728f99fa0fa2ccc16d740273911ded2828893653
Grégoire <gregoire.richard@hetic.net>
2021-03-29 16:06:12 +0200

    :sparkles: Views system is on

--- assembler.php
+++ assembler.php
@@ -2,11 +2,13 @@
     $baseUrl = "Url-Shortener";
     $bdd = new PDO('mysql:host=127.0.0.1;dbname=URLmembers','root', '');
     $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $resultUrl = $bdd->query("SELECT Shorten, Active, Views, Original FROM urllinks");
+    $resultUrl = $bdd->query("SELECT Shorten, Active, Views, Original, LinkID FROM urllinks");
     while($row = $resultUrl->fetch(PDO::FETCH_ASSOC)){
         $queryUrl = "/" . $baseUrl . "/". $row['Shorten'];
         if($_SERVER['REQUEST_URI'] == $queryUrl && $row['Active']){
-            setcookie("queryUrlCookie", $row['Shorten'], time() + (60),"/");
+            $Shorten = $row['LinkID'];
+            $resultViews = $bdd->prepare("UPDATE urllinks SET Views = Views + 1 WHERE LinkID = $Shorten" );
+            $resultViews->execute();
             $row['Views']++;
             header("location: ". $row['Original']);
         }
