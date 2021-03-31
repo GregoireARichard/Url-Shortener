920799dd6a77374fa86e87153beb062fbfb2f460
Grégoire <gregoire.richard@hetic.net>
2021-03-31 22:21:33 +0200

    :ambulance: removed useless files

--- pages/a75fa0.php
+++
@@ -1,5 +0,0 @@
-<?php 
-                    header("location: " . $_COOKIE["UrlCookie"]);
-                    $fileName = basename($_SERVER["PHP_SELF"]);
-                    unlink($fileName);
-        ?> 
