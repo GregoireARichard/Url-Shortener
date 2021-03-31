dd10353b4bea6ea1e150c3b05e4acf50ee468527
Grégoire <gregoire.richard@hetic.net>
2021-03-31 19:42:36 +0200

    :ambulance: Links fixed in header404

--- pages/header404.php
+++ pages/header404.php
@@ -2,8 +2,8 @@
     <div class="headerText">
         <a href="index.php"><img src="../img/logo.png" title="logo" alt="logo mnlnk" class="logoImg"/></a>
         <div class="linksWrapper">
-            <div class="signin"><a href="pages/connexion.php" class="signinLink">SIGN IN</a></div>
-            <div class="signup"><a href="pages/signup.php" class="signupLink">SIGN UP</a></div>
+            <div class="signin"><a href="connexion.php" class="signinLink">SIGN IN</a></div>
+            <div class="signup"><a href="signup.php" class="signupLink">SIGN UP</a></div>
         </div>
     </div>
 </div>
